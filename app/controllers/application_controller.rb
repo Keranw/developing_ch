51eeec86778df1b70184fa0e0cdfcbeb835b26dc
Keran <keranwang100@gmail.com>
2017-01-16 14:57:16 +1100

    fix url bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  #$host = 'ch-exp.herokuapp.com/'
-  $host = '10.0.4.85:3000'
+  $host = 'ch-exp.herokuapp.com/'
+  #$host = '10.0.4.85:3000'
 end
